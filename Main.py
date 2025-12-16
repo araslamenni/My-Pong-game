f05c3b88965e3d66f2d6d53792bad220c15b8ba8
Aras Lamenni <manismi2me@gmail.com>
2025-12-16 07:36:15 +0700

    add ball.py and detect collision with wall and bounce

--- Main.py
+++ Main.py
@@ -1,33 +1,38 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
-screen.title("My Pong game")
+screen.title("Pong game ")
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.setup(800, 600)
+screen.tracer(0)
 
-
-def up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def down():
-    new_y = paddle.ycor() + -20
-    paddle.goto(paddle.xcor(), new_y)
-
+l_paddle = Paddle((360, 0))
+r_paddle = Paddle((-360, 0))
+ball = Ball()
 
 screen.listen()
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
+screen.onkey(l_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "w")
+screen.onkey(r_paddle.go_down, "s")
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.penup()
-paddle.speed("fastest")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-# padel.turtlesize(stretch_wid=10, stretch_len=10)
-x_pos = 350
-y_pos = 0
-paddle.goto(x_pos, y_pos)
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 275 or ball.ycor() < -275 or ball.xcor() > 380 or ball.xcor() < -380:
+        ball.bounce()
+
+
+
+
+
 
 screen.exitonclick()
