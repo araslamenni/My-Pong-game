bac8a2dd6e011513bda72e89f3d7583e9c7cfdb7
Aras Lamenni <manismi2me@gmail.com>
2025-12-19 08:13:17 +0700

    detect r and l paddle misses

--- Main.py
+++ Main.py
@@ -22,7 +22,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(0.02)
     screen.update()
     ball.move()
 
@@ -34,7 +34,12 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    # else:
-    #     game_is_on = False
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        # game_is_on = False
 
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 screen.exitonclick()
